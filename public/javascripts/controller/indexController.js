35f63914b39d31c632d37e3262a2feeca1949f9b
adgzl-emn <adgzlemn51@gmail.com>
2024-02-12 00:01:29 +0300

    node edit socket

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,14 +1,33 @@
 app.controller('indexController',['$scope' , 'indexFactory' , ($scope , indexFactory) => {
 
-    const connectOptions = {
+    $scope.init = () => {
+        const username = prompt('Please Enter Username');
 
+        if (username)
+            initSocket(username);
+        else
+            return false;
     }
 
-    indexFactory.connectSocket('http://localhost:3000',connectOptions)
-        .then((socket) => {
-            console.log('Baglanti Gerceklesti');
-        }).catch((err) => {
+    const connectOptions = {
+        reconnectionAttempts: 3,  //kac kere baglanmayı denesın
+        reconnectionDelay: 600    //kac ms aralıklar ıle denesın
+    }
+
+    function initSocket(username) {
+        indexFactory.connectSocket('http://localhost:3000',connectOptions)
+            .then((socket) => {
+                console.log(username + ' Baglantiyi Gerceklesti',socket);
+                socket.emit('newUser', { username });
+            }).catch((err) => {
             console.log(err);
-    })
+        });
+    }
+
+
+
+
+
+
 
 }]);
